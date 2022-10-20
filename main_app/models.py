79b124db824e727848137a7c14e9487c1e35a9ff
Robert Ford <robert.ford1996@gmail.com>
2022-10-20 15:03:53 -0400

    adds details template

--- main_app/models.py
+++ main_app/models.py
@@ -3,21 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class Patient:
-    def __init__(self, name, address, email, age, reason, checkin):
-        self.name = name
-        self.address = address
-        self.email = email
-        self.age = age
-        self.reason = reason
-        self.checkin = checkin
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    age = models.IntegerField()
+    reason = models.TextField(max_length=250)
+    checkin = models.DateField()
 
 
-patients = [
-    Patient('Mark Monday', '123 Sesame Street',
-            'MarkMon@gmail.com', 53, 'Heart Attack', '2022-10-10'),
-    Patient('Terry Tuesday', '45 Maryland Drive',
-            'TerryTues@gmail.com', 16, 'Broken Arm', '2022-10-16'),
-    Patient('Wanda Wednesday', '43 Jamison Way', 'WandaWed@gmail.com',
-            72, 'Lymphatic Cancer', '2022-9-30'),
-]
+def __str__(self):
+    return self.name
